3e1d3098ea9881c7e9a8515511d7d3669236aba2
Thomas Roddick <somahtr@aol.com>
2018-12-06 15:19:56 +0000

    Add wavefront dataset class and dataset utilities

---
+++ aopwc/data.py
@@ -0,0 +1,87 @@
+import os
+import torch
+import numpy as np
+from torch.utils.data import Dataset
+
+# Statistics calculated over phase_screens_part1, see 
+# scripts/normalize_phase_screens.py for more details
+WAVEFRONT_MEAN = -9.48
+WAVEFRONT_STD = 2163.02 
+
+class WavefrontDataset(Dataset):
+    """
+    Dataset class for loading wavefront sequences
+
+    Args:
+        directory (str): path to folder containing npy files
+    """
+
+    def __init__(self, directory):
+        self.directory = os.path.abspath(directory)
+        self.filenames = sorted(os.listdir(directory))
+    
+    def __len__(self):
+        return len(self.filenames)
+    
+    def __getitem__(self, index):
+        if index >= len(self):
+            raise IndexError()
+        
+        path = os.path.join(self.directory, self.filenames[index])
+        return torch.from_numpy(np.load(path))
+
+
+class SubDataset(Dataset):
+    """
+    A dataset class representing a subset of another dataset
+
+    Args:
+        dataset (Dataset): the parent dataset
+        indices (list or np.ndarray): an array of indices representing the 
+            subset of the parent dataset
+    """
+
+    def __init__(self, dataset, indices):
+        self.dataset = dataset
+        self.indices = np.array(indices)
+    
+    def __len__(self):
+        return len(self.indices)
+    
+    def __getitem__(self, index):
+        if index >= len(self):
+            raise IndexError()
+        return self.dataset[self.indices[index]]
+
+
+def split_dataset(dataset, split_sizes, shuffle=True):
+    """
+    Splits a dataset into subsets of the specified size.
+
+    Args:
+        dataset (Dataset): the dataset to split
+        split_sizes (tuple of floats): iterable of floats in [0, 1] representing
+            what proportion of the dataset should be assigned to each subset
+        shuffle (bool): randomly shuffle the dataset when splitting
+
+    Returns:
+        splits (tuple of SubDataset): a tuple of datasets containing each 
+            subset of the data 
+    """
+
+    if shuffle:
+        # Generate a random permutation of the dataset indices
+        indices = np.random.randperm(len(dataset))
+    else:
+        indices = np.arange(len(dataset))
+    
+    # Split index array into chunks of the specified size
+    split_sizes = np.array(split_sizes).cumsum() * len(dataset)
+    split_inds = np.split(indices, split_sizes)[:len(split_sizes)]
+
+    # Return subset datasets
+    return [SubDataset(dataset, inds) for inds in split_inds]
+
+    
+
+    
