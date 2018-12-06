e7a0b468577f2524bb7e9c8a74790e1a75c39df4
Thomas Roddick <somahtr@aol.com>
2018-12-06 11:39:42 +0000

    Implement a sumple stacked LSTM model

--- aopwc/models/convrnn.py
+++ aopwc/models/convrnn.py
@@ -3,11 +3,27 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class ConvLSTMCell(nn.Module):
+    """
+    Implementation of a single Long Short-Term Memory unit.
 
+    Args:
+        input_channels (int): number of input features
+        hidden_channels (int): number of output features
+        kernel_size (int or tuple): height and width of 2D convolutional kernel
+        padding (int or tuple): amount of padding to add to the top/bottom and
+            left/right of the image. Use `(kernel_size-1)/2` to make the output
+            size the same as the input
+        bias (bool): if true, adds a learnable bias to the output
+
+    See the pytorch docs on convolution and LSTMs for more details:
+        https://pytorch.org/docs/stable/nn.html#conv2d
+        https://pytorch.org/docs/stable/nn.html#lstmcell
+    """
+    
     def __init__(self, input_channels, hidden_channels, kernel_size=3, 
                  padding=1, bias=True):
         super(ConvLSTMCell, self).__init__()
-        
+
         self.hidden_chls = hidden_channels 
         self.conv = nn.Conv2d(input_channels + hidden_channels, 
                               hidden_channels * 4,
@@ -36,3 +52,49 @@ class ConvLSTMCell(nn.Module):
         hidden = output_gate * F.tanh(cell_state)
 
         return hidden, cell_state
+
+
+class ConvLSTM(nn.Module):
+    """
+    A simple stacked convolutional LSTM model.
+
+    Args:
+        hidden_channels (list): containing the output feature size for each of
+            the LSTM layers 
+    """
+    def __init__(self, hidden_channels):
+        super(ConvLSTM, self).__init__()
+
+        # Create LSTM layers
+        in_channels = 1
+        self.layers = nn.ModuleList()
+        for out_channels in hidden_channels:
+            self.layers.append(ConvLSTMCell(in_channels, out_channels))
+            in_channels = out_channels
+
+        # Final prediction layer
+        self.conv = nn.Conv2d(in_channels, 1, kernel_size=3, padding=1)
+    
+
+    def forward(self, sequence):
+        
+        # Initialize hidden state
+        hidden = [None] * len(self.layers)
+        cell = [None] * len(self.layers)
+
+        # Iterate over frames
+        predictions = list()
+        for frame in torch.unbind(sequence, dim=1):
+
+            # Apply ConvLSTM layers
+            x = frame.unsqueeze(1)
+            for l, layer in enumerate(self.layers):
+                hidden[l], cell[l] = layer(x, hidden[l], cell[l])
+                x = hidden[l]
+            
+            # Generate final prediction
+            output = self.conv(x).squeeze(1)
+            predictions.append(output)
+
+        # Combine predictions into a single tensor
+        return torch.stack(predictions, dim=1) 
